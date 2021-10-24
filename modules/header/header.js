ec8e74a5c96e24545a9819f613eb184668608e4f
MarijaM67 <mmatic02@fesb.hr>
2021-10-24 10:36:08 +0200

    adding header

---
+++ modules/header/header.js
@@ -0,0 +1,24 @@
+import styles from "./header.module.css";
+import Image from "next/image";
+
+import LogoImg from "../../assets/logo.png";
+
+const Header = () => {
+  return (
+    <header className={styles.main}>
+      <main className={styles.content}>
+        <Image src={LogoImg} />
+        <nav>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Showcase</li>
+          <li>Blog</li>
+          <li>Contact us</li>
+          <li>Sign in</li>
+        </nav>
+      </main>
+    </header>
+  );
+};
+
+export default Header;
